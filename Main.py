29e5a1b0ef816a61a682ad9edf9bbbc41a8ffad3
PKOWORKSTATION\adrim <adrimonge2014@gmail.com>
2024-10-19 21:28:28 -0600

    modified:   Main.py

--- Main.py
+++ Main.py
@@ -13,7 +13,7 @@ reloj = pygame.time.Clock()
 
 # Sonido y música
 pygame.mixer.music.load("Resources/Audio/Dragonball Z Budokai Tenkaichi 3 OpeningIntroHD.mp3")
-pygame.mixer.music.set_volume(0.3)
+pygame.mixer.music.set_volume(30)
 pygame.mixer.music.play(-1)
 
 # Función para mostrar la pantalla de inicio
@@ -52,17 +52,41 @@ hilo_recibir = threading.Thread(target=recibir_datos)
 hilo_recibir.daemon = True
 hilo_recibir.start()
 
-# Cargar imágenes de la animación
-imagenes_animacion = [pygame.image.load(f"animacion/frame_{i}.png") for i in range(1, 11)]
+# Lista para almacenar las imágenes de la animación
+imagenes_animacion = []
 indice_animacion = 0
-pos_x, pos_y = ancho // 2, alto // 2
+imagenes_cargadas = False
+
+# Función para cargar imágenes de la animación
+def cargar_imagenes():
+    global imagenes_animacion, imagenes_cargadas
+    imagenes_animacion = [pygame.image.load(f"Resources/animacion/{i}.png") for i in range(1, 1314)]
+    # Obtener el tamaño de la primera imagen para centrar todas las imágenes
+    global ancho_imagen, alto_imagen, pos_x, pos_y
+    ancho_imagen, alto_imagen = imagenes_animacion[0].get_size()
+    pos_x = (ancho - ancho_imagen) // 2
+    pos_y = (alto - alto_imagen) // 2
+    imagenes_cargadas = True
+
+# Hilo para cargar imágenes
+hilo_cargar_imagenes = threading.Thread(target=cargar_imagenes)
+hilo_cargar_imagenes.start()
 
 # Función para actualizar y dibujar la animación
 def actualizar_animacion():
     global indice_animacion
-    pantalla.fill((0, 0, 0))  # Limpiar la pantalla
-    pantalla.blit(imagenes_animacion[indice_animacion], (pos_x, pos_y))
-    indice_animacion = (indice_animacion + 1) % len(imagenes_animacion)
+    if imagenes_animacion:
+        pantalla.fill((0, 0, 0))  # Limpiar la pantalla
+        pantalla.blit(imagenes_animacion[indice_animacion], (pos_x, pos_y))
+        indice_animacion = (indice_animacion + 1) % len(imagenes_animacion)
+        pygame.display.flip()
+
+# Función para mostrar la pantalla de carga
+def pantalla_carga():
+    pantalla.fill((0, 0, 0))
+    fuente = pygame.font.Font(None, 74)
+    texto = fuente.render("Cargando...", True, (255, 255, 255))
+    pantalla.blit(texto, (ancho // 2 - texto.get_width() // 2, alto // 2))
     pygame.display.flip()
 
 # Bucle principal del juego
@@ -73,7 +97,11 @@ while ejecutando:
         if evento.type == pygame.QUIT:
             ejecutando = False
 
-    actualizar_animacion()
-    reloj.tick(10)  # Controlar la velocidad de la animación
+    if imagenes_cargadas:
+        actualizar_animacion()
+    else:
+        pantalla_carga()
+
+    reloj.tick(30)  # Controlar la velocidad de la animación
 
 pygame.quit()
